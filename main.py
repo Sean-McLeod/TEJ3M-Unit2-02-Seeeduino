1d18ec1121bbb4c210a32f22e80e74f30a313028
sean <sean.mcleod@mths.ca>
2020-09-24 23:04:12 -0400

    initial message

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+import board
+from digitalio import DigitalInOut, Direction
+ 
+led = DigitalInOut(board.D13)
+led.direction = Direction.OUTPUT
+lightTime = 5
+while True:
+    led.value = True
+    time.sleep(lightTime)
+    led.value = False
+    time.sleep(1)
